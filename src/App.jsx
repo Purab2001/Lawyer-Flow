a997435ab7e73f4fe529649e96d94deb62bd3406
Abir Shahadat Purab <a.s.purab0@gmail.com>
2025-05-12 01:07:37 +0600

    feat: Set up main application structure with routing and page components; add _redirects for index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import './App.css'
+import { useState } from 'react'
+import Main from './layout/Main'
+import Home from './pages/Home'
+import Bookings from './pages/Bookings'
+import Blogs from './pages/Blogs'
+import LawyerDetails from './pages/LawyerDetails'
+import ErrorPage from './pages/ErrorPage'
+import LoadingSpinner from './ui/LoadingSpinner'
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home')
+  const [lawyerId, setLawyerId] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home onLawyerSelect={(id) => {
+          setLawyerId(id)
+          setCurrentPage('lawyer-details')
+        }} />
+      case 'bookings':
+        return <Bookings />
+      case 'blogs':
+        return <Blogs />
+      case 'lawyer-details':
+        return <LawyerDetails licenseNumber={lawyerId} />
+      default:
+        return <ErrorPage />
+    }
+  }
 
   return (
-    <>
-    </>
+    <Main onNavigate={setCurrentPage}>
+      {isLoading ? <LoadingSpinner /> : renderPage()}
+    </Main>
   )
 }
 
